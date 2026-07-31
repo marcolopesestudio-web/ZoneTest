448200a67de8f3b2e3a34258761580db1e8d951c
marcolopesestudio-web <marcolopes.estudio@gmail.com>
2026-07-31 14:24:48 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburgerBtn = document.getElementById('hamburgerBtn');
-const navMenu = document.getElementById('navMenu');
-
-hamburgerBtn.addEventListener('click', () => {
-    hamburgerBtn.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
